90562c3f5f5cb21061b1b135225700af8b2c8b8f
codefuncode <codefuncode@protonmail.com>
2020-05-17 13:55:23 -0400

    Modulo 7 para revisar  textos selacinados agregados al documento de analisis

--- 001-Conceptos-introductorios-a-las-bases-de-datos/002-Una-base-de-datos-de-muestra/php/php.php
+++ 001-Conceptos-introductorios-a-las-bases-de-datos/002-Una-base-de-datos-de-muestra/php/php.php
@@ -1,1 +1,29 @@
+<?php
 
+/**
+ * summary
+ */
+class Conexion
+{
+
+    public function __construct($s, $u, $p, $d)
+    {
+        $this->servername = $s;
+        $this->dbname     = $d;
+        $this->username   = $u;
+        $this->password   = $p;
+
+        try {
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e) {
+
+            echo "Connection failed: " . $e->getMessage();
+        }
+
+    }
+}
+
+$nueva = new Conexion('localhost', 'root', '', '002-Una-base-de-datos-de-muestra');
